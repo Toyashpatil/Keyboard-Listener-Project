1778d42813a50ad334b68b310595a2756c7df844
Toyash <toyashpatil17@gmail.com>
2023-05-20 21:40:12 +0530

    Changed Js sounds paths

--- index.js
+++ index.js
@@ -4,107 +4,107 @@ document.addEventListener("keypress",function(event){
     var press=event.key
     switch(press){
         case 'q':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/q.mp3");
+            var audio=new Audio("sounds/q.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'w':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/w.mp3");
+            var audio=new Audio("sounds/w.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'e':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/e.mp3");
+            var audio=new Audio("sounds/e.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'r':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/r.mp3");
+            var audio=new Audio("sounds/r.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 't':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/t.mp3");
+            var audio=new Audio("sounds/t.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 't':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/y.mp3");
+            var audio=new Audio("sounds/y.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'y':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/u.mp3");
+            var audio=new Audio("sounds/u.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'i':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/i.mp3");
+            var audio=new Audio("sounds/i.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'o':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/o.mp3");
+            var audio=new Audio("sounds/o.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'p':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/p.mp3");
+            var audio=new Audio("sounds/p.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'a':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/a.mp3");
+            var audio=new Audio("sounds/a.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 's':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/s.mp3");
+            var audio=new Audio("sounds/s.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'd':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/d.mp3");
+            var audio=new Audio("sounds/d.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'f':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/f.mp3");
+            var audio=new Audio("sounds/f.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'g':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/g.mp3");
+            var audio=new Audio("sounds/g.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'h':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/h.mp3");
+            var audio=new Audio("sounds/h.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'j':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/j.mp3");
+            var audio=new Audio("sounds/j.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'k':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/k.mp3");
+            var audio=new Audio("sounds/k.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'l':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/l.mp3");
+            var audio=new Audio("sounds/l.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'z':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/z.mp3");
+            var audio=new Audio("sounds/z.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'x':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/x.mp3");
+            var audio=new Audio("sounds/x.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'c':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/c.mp3");
+            var audio=new Audio("sounds/c.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'v':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/v.mp3");
+            var audio=new Audio("sounds/v.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'b':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/b.mp3");
+            var audio=new Audio("sounds/b.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'n':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/n.mp3");
+            var audio=new Audio("sounds/n.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         case 'm':
-            var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/m.mp3");
+            var audio=new Audio("sounds/m.mp3");
             audio.play()/*alert("Button Pressesd")*/;
             break;
         default:
@@ -123,107 +123,107 @@ for(var i=0;i<nb;i++){
         var clicked=this.innerHTML
         switch(clicked){
             case 'Q':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/q.mp3");
+                var audio=new Audio("sounds/q.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'W':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/w.mp3");
+                var audio=new Audio("sounds/w.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'E':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/e.mp3");
+                var audio=new Audio("sounds/e.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'R':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/r.mp3");
+                var audio=new Audio("sounds/r.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'T':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/t.mp3");
+                var audio=new Audio("sounds/t.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'Y':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/y.mp3");
+                var audio=new Audio("sounds/y.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'U':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/u.mp3");
+                var audio=new Audio("sounds/u.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'I':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/i.mp3");
+                var audio=new Audio("sounds/i.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'O':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/o.mp3");
+                var audio=new Audio("sounds/o.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'P':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/p.mp3");
+                var audio=new Audio("sounds/p.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'A':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/a.mp3");
+                var audio=new Audio("sounds/a.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'S':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/s.mp3");
+                var audio=new Audio("sounds/s.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'D':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/d.mp3");
+                var audio=new Audio("sounds/d.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'F':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/f.mp3");
+                var audio=new Audio("sounds/f.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'G':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/g.mp3");
+                var audio=new Audio("sounds/g.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'H':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/h.mp3");
+                var audio=new Audio("sounds/h.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'J':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/j.mp3");
+                var audio=new Audio("sounds/j.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'K':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/k.mp3");
+                var audio=new Audio("sounds/k.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'L':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/l.mp3");
+                var audio=new Audio("sounds/l.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'Z':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/z.mp3");
+                var audio=new Audio("sounds/z.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'X':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/x.mp3");
+                var audio=new Audio("sounds/x.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'C':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/c.mp3");
+                var audio=new Audio("sounds/c.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'V':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/v.mp3");
+                var audio=new Audio("sounds/v.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'B':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/b.mp3");
+                var audio=new Audio("sounds/b.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'N':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/n.mp3");
+                var audio=new Audio("sounds/n.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             case 'M':
-                var audio=new Audio("D:/All Coding stuff/Keyboard Listner/sounds/m.mp3");
+                var audio=new Audio("sounds/m.mp3");
                 audio.play()/*alert("Button Pressesd")*/;
                 break;
             default:
